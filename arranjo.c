43cf76df55d729546e268b36c07df93ed65b477b
Thiago Marcelo Marques-TMARQUES <tmarques@motorola.com>
2023-02-26 22:20:07 -0400

    adicionando código de permutação

--- arranjo.c
+++ arranjo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 // Calcula o fatorial de n
 int fatorial(int n)
@@ -26,9 +27,18 @@ int arranjo_simples_alternativo(int n, int p)
     return result;
 }
 
+// calcula a formula n^p
+int arranjo_com_repeticao(int n, int p)
+{
+    return pow(n, p);
+}
+
 int main()
 {
     // Arranjo de 4 itens, 3 a 3.
     arranjo_simples(4, 3);
     arranjo_simples_alternativo(4, 3);
+
+    // Arranjo de 3 itens, 2 a 2 com repetição
+    arranjo_com_repeticao(3, 2);
 }
---
+++ permutacao.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+// Calcula o fatorial de n
+int fatorial(int n)
+{
+    int result = 1;
+    for (int i = n; i > 1; i--)
+        result *= i;
+    return result;
+}
+
+// calcula a formula -> n!
+int permutacao_simples(int n)
+{
+    return fatorial(n);
+}
+
+int permutacao_com_repeticao(int n, int values[], int values_size)
+{
+    int aux = 1;
+    for (int i = 0; i < values_size; i++)
+    {
+        aux *= fatorial(values[i]);
+    }
+
+    return fatorial(n) / aux;
+}
+
+int main()
+{
+    // permutação simples
+    permutacao_simples(5);
+
+    // permutação com repetição
+    // quantidade de vezes que cada um dos valores se repete
+    int values[] = {3, 2, 1};
+    // tamanho do vetor acima
+    int qnt = 3;
+    permutacao_com_repeticao(6, values, qnt);
+}
